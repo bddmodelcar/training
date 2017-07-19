ccb2f81734413a7c5ff3701f8e8408d8ba36612c
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2017-07-18 17:35:34 -0700

    made some progress on main loop rewrite

--- Train.py
+++ Train.py
@@ -32,55 +32,41 @@ rate_counter = Utils.Rate_Counter()
 
 data = Data.Data()
 
-timer = {}
-timer['train'] = Timer(args.mini_train_time)
-timer['val'] = Timer(args.mini_val_time)
-print_timer = Timer(args.print_time)
-save_timer = Timer(args.save_time)
-
 # Maitains a list of all inputs to the network, and the loss and outputs for
 # each of these runs. This can be used to sort the data by highest loss and
 # visualize, to do so run:
 # display_sort_trial_loss(data_moment_loss_record , data)
+mini_train_percentage = 25
 data_moment_loss_record = {}
 
-batch = Batch.Batch(net)
+def run_net(data_index):
+    batch.fill(data, data_index)  # Get batches ready
+    batch.forward(optimizer, criterion, data_moment_loss_record)
 
 while True:
-    for mode, data_index in [('train', data.train_index),
-                             ('val', data.val_index)]:
-        timer[mode].reset()
-        while not timer[mode].check():
+    run_net(data.train_index)  # Run network
+    batch.backward(optimizer)  # Backpropagate
+    train_loss_record.add(batch.loss.data[0])  # Record loss
 
-            batch.fill(data, data_index)  # Get batches ready
+    if ctr < 0:
+        rate_counter.step()
 
-            # Run net, forward pass
-            batch.forward(optimizer, criterion, data_moment_loss_record)
+        if data_index.epoch_complete and mode == 'train':
+            
+            Utils.save_net(net, loss_record, weights_prefix='epoch_' +
+                           str(data_index.epoch_counter - 1) + '_')
+            data_index.epoch_complete = False
+            break 
 
-            if mode == 'train':  # Backpropagate
-                batch.backward(optimizer)
-
-            loss_record[mode].add(batch.loss.data[0])
-            rate_counter.step()
-
-            if save_timer.check():
-                Utils.save_net(net, loss_record)
-                save_timer.reset()
-
-            if mode == 'train' and data_index.epoch_complete:
-                Utils.save_net(net, loss_record, weights_prefix='epoch_' +
-                               str(data_index.epoch_counter - 1) + '_')
-                data_index.epoch_complete = False
-
-            if print_timer.check():
-                print(d2n('mode=',mode,
-                    ',ctr=',data_index.ctr,
-                    ',epoch progress=',dp(100*data_index.ctr / (len(data_index.valid_data_moments)*1.0)),
-                    ',epoch=',data_index.epoch_counter))
-                if args.display:
-                    batch.display()
-                    plt.figure('loss')
-                    plt.clf()  # clears figure
-                    loss_record['train'].plot('b')  # plot with blue color
-                    loss_record['val'].plot('r')  # plot with red color
-                    print_timer.reset()
+        if print_timer.check():
+            print(d2n('mode=',mode,
+                ',ctr=',data_index.ctr,
+                ',epoch progress=',dp(100*data_index.ctr / (len(data_index.valid_data_moments)*1.0)),
+                ',epoch=',data_index.epoch_counter))
+            if args.display:
+                batch.display()
+                plt.figure('loss')
+                plt.clf()  # clears figure
+                loss_record['train'].plot('b')  # plot with blue color
+                loss_record['val'].plot('r')  # plot with red color
+                print_timer.reset()
