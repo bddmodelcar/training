74775b5549da4b5d71b7fa6ca904642f69b31ec3
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2017-08-15 19:03:05 -0700

    LSTM version of SqueezeNet
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

---
+++ nets/SqueezeNetLSTM.py
@@ -0,0 +1,95 @@
+"""SqueezeNet 1.1 modified for LSTM regression."""
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from torch.autograd import Variable
+import logging
+logging.basicConfig(filename='training.log', level=logging.DEBUG)
+
+#from Parameters import ARGS
+
+class Fire(nn.Module):
+
+    def __init__(self, inplanes, squeeze_planes,
+                 expand1x1_planes, expand3x3_planes):
+        super(Fire, self).__init__()
+        self.inplanes = inplanes
+        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.ReLU(inplace=True)
+        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
+                                   kernel_size=1)
+        self.expand1x1_activation = nn.ReLU(inplace=True)
+        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
+                                   kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.squeeze_activation(self.squeeze(x))
+        return torch.cat([
+            self.expand1x1_activation(self.expand1x1(x)),
+            self.expand3x3_activation(self.expand3x3(x))
+        ], 1)
+
+
+class SqueezeNetLSTM(nn.Module):
+
+    def __init__(self):
+        super(SqueezeNetLSTM, self).__init__()
+
+        self.lr = 0.01
+        self.momentum = 0.01
+        self.N_STEPS = 10
+        self.pre_metadata_features = nn.Sequential(
+            nn.Conv2d(2 * 6, 64, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(64, 16, 64, 64),
+        )
+        self.post_metadata_features = nn.Sequential(
+            Fire(256, 16, 64, 64),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(128, 32, 128, 128),
+            Fire(256, 32, 128, 128),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(256, 48, 192, 192),
+            Fire(384, 48, 192, 192),
+            Fire(384, 64, 256, 256),
+            Fire(512, 64, 256, 256),
+        )
+        final_conv = nn.Conv2d(512, self.N_STEPS * 2, kernel_size=1)
+        self.pre_lstm_output = nn.Sequential(
+            nn.Dropout(p=0.5),
+            final_conv,
+            nn.AvgPool2d(kernel_size=3, stride=2),
+        )
+        self.lstm = nn.LSTM(16, 2, 8, batch_first=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                if m is final_conv:
+                    init.normal(m.weight.data, mean=0.0, std=0.01)
+                else:
+                    init.kaiming_uniform(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+    def forward(self, x, metadata):
+        x = self.pre_metadata_features(x)
+        x = torch.cat((x, metadata), 1)
+        x = self.post_metadata_features(x)
+        x = self.pre_lstm_output(x)
+        x = x.view(x.size(0), self.N_STEPS, -1)
+        x = self.lstm(x)[0]
+        x = x.contiguous().view(x.size(0), -1)
+        return x
+
+
+def unit_test():
+    test_net = SqueezeNetLSTM()
+    a = test_net(Variable(torch.randn(5, 2 * 6, 94, 168)),
+                 Variable(torch.randn(5, 128, 23, 41)))
+    logging.debug('Net Test Output = {}'.format(a))
+    logging.debug('Network was Unit Tested')
+
+
+unit_test()
