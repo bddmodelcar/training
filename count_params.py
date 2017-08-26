d2746064dfdcd14a351324c7f5f4a0156198f152
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2017-08-26 13:56:27 -0700

    Fix Codacy errors
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- count_params.py
+++ count_params.py
@@ -1,9 +1,10 @@
+"""Script to count the number of parameters in a PyTorch state_dict"""
 import sys
 import numpy as np
 import torch
 
-model = torch.load(sys.argv[1])
-params = 0
-for key in model:
-    params += np.multiply.reduce(model[key].shape)
+MODEL = torch.load(sys.argv[1])
+params = 0 # pylint: disable=invalid-name
+for key in MODEL:
+    params += np.multiply.reduce(MODEL[key].shape)
 print('Total number of parameters: ' + str(params))
