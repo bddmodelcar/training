00892dee96f8cf2a15fcd3516f8de5400edd4bb6
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2017-09-08 14:39:41 -0700

    More detailed printouts

--- Train.py
+++ Train.py
@@ -2,9 +2,11 @@
 import sys
 import traceback
 import logging
+import time
 
 from Parameters import ARGS
-from Dataset import Dataset
+# from Dataset import Dataset
+from ArucoDataset import Dataset
 import Utils
 
 import matplotlib.pyplot as plt
@@ -41,13 +43,13 @@ def main():
 
         net.train()  # Train mode
 
-        train_dataset = Dataset('/hostroot/data/dataset/bair_car_data_Main_Dataset', ARGS.require_one, ARGS.ignore)
+        train_dataset = Dataset('/hostroot/data/dataset/bair_car_data_new_28April2017', ARGS.require_one, ARGS.ignore)
         train_data_loader = torch.utils.data.DataLoader(train_dataset,
                                                         batch_size=500,
                                                         shuffle=False, pin_memory=False)
 
         train_loss = Utils.LossLog()
-
+        start = time.time()
         for batch_idx, (camera, meta, truth, mask) in enumerate(train_data_loader):
             # Cuda everything
             camera = camera.cuda()
@@ -77,13 +79,17 @@ def main():
 		epoch, batch_idx * len(camera), len(train_data_loader.dataset),
 		100. * batch_idx / len(train_data_loader), loss.data[0]))
 
+            cur = time.time()
+            print(500./(cur - start))
+            start = cur
+
 
         Utils.csvwrite('trainloss.csv', [train_loss.average()])
 
         logging.debug('Finished training epoch #{}'.format(epoch))
         logging.debug('Starting validation epoch #{}'.format(epoch))
 
-        val_dataset = Dataset('/hostroot/data/dataset/bair_car_data_Main_Dataset', ARGS.require_one, ARGS.ignore)
+        val_dataset = Dataset('/hostroot/data/dataset/bair_car_data_new_28April2017', ARGS.require_one, ARGS.ignore)
         val_data_loader = torch.utils.data.DataLoader(val_dataset,
                                                         batch_size=500,
                                                         shuffle=False, pin_memory=False)
