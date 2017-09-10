f9114b62942c691720f6278d3633bb42a16cb060
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-09-09 23:31:29 -0700

    Finished SqueezeLSTM

--- nets/SqueezeNetSqueezeLSTM.py
+++ nets/SqueezeNetSqueezeLSTM.py
@@ -70,13 +70,15 @@ class SqueezeNetSqueezeLSTM(nn.Module):  # pylint: disable=too-few-public-method
             final_conv,
             nn.AvgPool2d(kernel_size=3, stride=2),
         )
-        self.lstm = nn.LSTM(64, 128, 1, batch_first=True)
-        self.squeeze_lstm1 = nn.LSTM(128, 32, 2, batch_first=True)
-        self.expand_lstm1 = nn.LSTM(32, 64, 1, batch_first=True)
-        self.squeeze_lstm2 = nn.LSTM(64, 16, 2, batch_first=True)
-        self.expand_lstm2 = nn.LSTM(16, 32, 1, batch_first=True)
-        self.squeeze_lstm3 = nn.LSTM(32, 8, 2, batch_first=True)
-        self.final_lstm = nn.LSTM(8, 2, 1, batch_first=True)
+        self.lstms = nn.ModuleList([
+            nn.LSTM(64, 128, 1, batch_first=True),
+            nn.LSTM(128, 32, 2, batch_first=True),
+            nn.LSTM(32, 64, 1, batch_first=True),
+            nn.LSTM(64, 16, 2, batch_first=True),
+            nn.LSTM(16, 32, 1, batch_first=True),
+            nn.LSTM(32, 8, 2, batch_first=True),
+            nn.LSTM(8, 2, 1, batch_first=True)
+        ])
 
         for mod in self.modules():
             if isinstance(mod, nn.Conv2d):
@@ -94,13 +96,8 @@ class SqueezeNetSqueezeLSTM(nn.Module):  # pylint: disable=too-few-public-method
         net_output = self.post_metadata_features(net_output)
         net_output = self.pre_lstm_output(net_output)
         net_output = net_output.view(net_output.size(0), self.n_steps, -1)
-        net_output = self.lstm(net_output)[0]
-        net_output = self.squeeze_lstm1(net_output)[0]
-        net_output = self.expand_lstm1(net_output)[0]
-        net_output = self.squeeze_lstm2(net_output)[0]
-        net_output = self.expand_lstm2(net_output)[0]
-        net_output = self.squeeze_lstm3(net_output)[0]
-        net_output = self.final_lstm(net_output)[0]
+        for lstm in self.lstms:
+            net_output = lstm(net_output)[0]
         net_output = net_output.contiguous().view(net_output.size(0), -1)
         return net_output
 
