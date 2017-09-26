fc5314f0f358e9b01042600e9829392fb40cd505
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-09-26 00:09:05 -0700

    Changed nframes

--- Dataset.py
+++ Dataset.py
@@ -13,7 +13,7 @@ from random import shuffle
 
 class Dataset(data.Dataset):
     
-    nframes = 2
+    nframes = 8
 
     def __init__(self, data_folder_dir, require_one, ignore_list, stride=10, max_len=-1):
         self.max_len = max_len
