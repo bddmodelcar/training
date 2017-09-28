0b5e9c2a9f382a7988438d5821fa765b96dc56e4
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-09-27 17:49:43 -0700

    Changed train net

--- TrainLSTM.py
+++ TrainLSTM.py
@@ -26,7 +26,7 @@ def main():
     torch.cuda.set_device(ARGS.gpu)
     torch.cuda.device(ARGS.gpu)
 
-    net = SqueezeNetSqueezeLSTM().cuda()
+    net = SqueezeNetTimeLSTM().cuda()
     criterion = torch.nn.MSELoss().cuda()
     optimizer = torch.optim.Adam(net.parameters())
 
