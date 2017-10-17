9ad7ffe045863cb2cc6e4e8753271bf355ced835
Eric (Yiqi) Hou <erichou2004@gmail.com>
2017-10-17 11:05:56 -0700

    Nvidia metadata model

--- nets/eric/nonsqueeze/NvidiaMetadata.py
+++ nets/eric/nonsqueeze/NvidiaMetadata.py
@@ -6,17 +6,19 @@ from torch.autograd import Variable
 import logging
 logging.basicConfig(filename='training.log', level=logging.DEBUG)
 
-class Nvidia(nn.Module):
+class NvidiaMetadata(nn.Module):
 
     def __init__(self, n_steps=10, n_frames=2):
-        super(Nvidia, self).__init__()
+        super(NvidiaMetadata, self).__init__()
 
         self.n_steps = n_steps
         self.n_frames = n_frames
-        self.conv_nets = nn.Sequential(
+        self.pre_metadata = nn.Sequential(
             nn.Conv2d(3 * 2 * self.n_frames, 24, kernel_size=5, stride=2),
-            nn.Conv2d(24, 36, kernel_size=5, stride=2),
-            nn.Conv2d(36, 48, kernel_size=5, stride=2),
+            nn.Conv2d(24, 36, kernel_size=5, stride=2)
+        )
+        self.post_metadata = nn.Sequential(
+            nn.Conv2d(48, 48, kernel_size=5, stride=2),
             nn.Conv2d(48, 64, kernel_size=3, stride=2),
             nn.Conv2d(64, 64, kernel_size=3, stride=1)
         )
@@ -27,7 +29,9 @@ class Nvidia(nn.Module):
 
 
     def forward(self, x, metadata):
-        x = self.conv_nets(x)
+        x = self.pre_metadata(x)
+        x = torch.cat([x, metadata], 1)
+        x = self.post_metadata(x)
         x = x.view(x.size(0), -1)
         x = self.fcl(x)
         x = x.view(x.size(0), -1, 4)
@@ -37,13 +41,13 @@ class Nvidia(nn.Module):
         return sum([reduce(lambda x, y: x * y, [dim for dim in p.size()], 1) for p in self.parameters()])
 
 def unit_test():
-    test_net = Nvidia(20, 6)
+    test_net = NvidiaMetadata(20, 6)
     a = test_net(Variable(torch.randn(5, 36, 94, 168)),
-                 Variable(torch.randn(5, 12, 23, 41)))
+                 Variable(torch.randn(5, 12, 21, 39)))
     logging.debug('Net Test Output = {}'.format(a))
     logging.debug('Network was Unit Tested')
     print(test_net.num_params())
 
 unit_test()
 
-Net = Nvidia
+Net = NvidiaMetadata
